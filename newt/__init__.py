051a5d8fa7531bcf9cf7181640de1c1c2799c40f
Kael Zhang <i+github@kael.me>
2020-06-22 17:16:47 +0800

    adds newt.png

--- newt/__init__.py
+++ newt/__init__.py
@@ -57,8 +57,7 @@ class PriorityQueue(_AbstractQueue[T]):
     """Variant of Queue that retrieves open entries in priority order
     (lowest first).
 
-    Entries are typically tuples of the form:  (priority number, data).
-
+    Entries are typically tuples of the form: (priority number, data).
     """
 
     _heap_queue: List[T]
@@ -77,7 +76,8 @@ class PriorityQueue(_AbstractQueue[T]):
 
 
 class LifoQueue(_AbstractQueue[T]):
-    """Variant of Queue that retrieves most recently added entries first."""
+    """Variant of Queue that retrieves most recently added entries first.
+    """
 
     _queue: Deque
 
