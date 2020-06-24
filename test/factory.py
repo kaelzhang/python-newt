fb2efb83f2ff095a516adde233b3412b0830ebb0
Kael Zhang <i+github@kael.me>
2020-06-24 23:13:05 +0800

    test: more test cases

--- test/factory.py
+++ test/factory.py
@@ -4,10 +4,18 @@ import time
 RANGE = 100
 
 
-def async_producer(sleep_on_every, sleep):
+def async_producer(
+    sleep_on_every,
+    sleep,
+    sequence,
+    init_delay
+):
     async def p(queue):
-        for i in range(RANGE):
-            await queue.put(i)
+        if init_delay:
+            await asyncio.sleep(init_delay)
+
+        for i, item in enumerate(sequence):
+            await queue.put(item)
 
             if (i + 1) % sleep_on_every == 0:
                 await asyncio.sleep(sleep)
@@ -17,10 +25,19 @@ def async_producer(sleep_on_every, sleep):
     return p
 
 
-def async_consumer(sleep_on_every, sleep):
+def async_consumer(
+    sleep_on_every,
+    sleep,
+    sequence,
+    init_delay
+):
     async def c(queue):
-        for i in range(RANGE):
-            assert await queue.get() == i
+        if init_delay:
+            await asyncio.sleep(init_delay)
+
+        for i, item in enumerate(sequence):
+            print(i, item)
+            assert await queue.get() == item
 
             if (i + 1) % sleep_on_every == 0:
                 await asyncio.sleep(sleep)
@@ -30,10 +47,18 @@ def async_consumer(sleep_on_every, sleep):
     return c
 
 
-def sync_producer(sleep_on_every, sleep):
+def sync_producer(
+    sleep_on_every,
+    sleep,
+    sequence,
+    init_delay
+):
     def p(queue):
-        for i in range(RANGE):
-            queue.put(i)
+        if init_delay:
+            time.sleep(init_delay)
+
+        for i, item in enumerate(sequence):
+            queue.put(item)
 
             if (i + 1) % sleep_on_every == 0:
                 time.sleep(sleep)
@@ -43,10 +68,18 @@ def sync_producer(sleep_on_every, sleep):
     return p
 
 
-def sync_consumer(sleep_on_every, sleep):
+def sync_consumer(
+    sleep_on_every,
+    sleep,
+    sequence,
+    init_delay
+):
     def c(queue):
-        for i in range(RANGE):
-            assert queue.get() == i
+        if init_delay:
+            time.sleep(init_delay)
+
+        for i, item in enumerate(sequence):
+            assert queue.get() == item
 
             if (i + 1) % sleep_on_every == 0:
                 time.sleep(sleep)
@@ -60,7 +93,9 @@ def create(
     consumer=False,
     is_async=False,
     sleep_on_every=RANGE,
-    sleep=0.1
+    sleep=0.1,
+    sequence=range(RANGE),
+    init_delay=0
 ):
     if consumer:
         if is_async:
@@ -73,4 +108,9 @@ def create(
         else:
             factory = sync_consumer
 
-    return factory(sleep_on_every, sleep)
+    return factory(
+        sleep_on_every,
+        sleep,
+        sequence,
+        init_delay
+    )
--- test/test_newt.py
+++ test/test_newt.py
@@ -15,38 +15,30 @@ from .runner import create_runner
 from .factory import create
 
 
-def map_options(options: Union[dict, bool]) -> dict:
+def map_options(options: Union[dict, bool], consumer: bool) -> dict:
     if type(options) is bool:
         return {
+            'consumer': consumer,
             'is_async': options
         }
 
-    return options
+    return {
+        **options,
+        'consumer': consumer
+    }
 
 
 def map_two_options(l: list):
     return [
-        (map_options(a), map_options(b))
+        (
+            map_options(False, a),
+            map_options(True, b)
+        )
         for a, b in l
     ]
 
 
-@pytest.mark.parametrize(
-    'queue_ctor,max_size,options',
-    itertools.product(
-        [
-            Queue,
-            PriorityQueue,
-            # LifoQueue
-        ],
-        [-1, 2],
-        map_two_options([
-            (True, False),
-            (False, True)
-        ])
-    )
-)
-def test_newt(
+def run(
     queue_ctor,
     max_size: int,
     options: Tuple[dict, dict]
@@ -63,3 +55,112 @@ def test_newt(
     runner = create_runner(producer_is_async)
 
     runner(queue, producer, consumer)
+
+
+@pytest.mark.parametrize(
+    'queue_ctor,max_size,options',
+    itertools.product(
+        [
+            Queue
+        ],
+        [-1, 2],
+        map_two_options([
+            (True, False),
+            (False, True),
+            (
+                {
+                    'is_async': True,
+                    'sleep_on_every': 2
+                },
+                {
+                    'is_async': False,
+                    'sleep_on_every': 3
+                }
+            )
+        ])
+    )
+)
+def test_queue(
+    queue_ctor,
+    max_size: int,
+    options: Tuple[dict, dict]
+):
+    run(queue_ctor, max_size, options)
+
+
+priority_queue_sequence = [
+    (2, 1),
+    (1, 0),
+    (4, 3),
+    (5, 4),
+    (6, 5),
+    (3, 2),
+]
+
+@pytest.mark.parametrize(
+    'queue_ctor,max_size,options',
+    itertools.product(
+        [
+            PriorityQueue
+        ],
+        [-1],
+        map_two_options([
+            (
+                {
+                    'is_async': True,
+                    'sequence': priority_queue_sequence
+                },
+                {
+                    'is_async': False,
+                    'sequence': sorted(
+                        priority_queue_sequence,
+                        key=lambda x: x[0]
+                    ),
+                    'init_delay': 0.2
+                }
+            )
+        ])
+    )
+)
+def test_priority_queue(
+    queue_ctor,
+    max_size: int,
+    options: Tuple[dict, dict]
+):
+    run(queue_ctor, max_size, options)
+
+
+lifo_queue_sequence = range(100)
+lifo_queue_sequence_got = list(lifo_queue_sequence)
+
+lifo_queue_sequence_got.reverse()
+
+
+@pytest.mark.parametrize(
+    'queue_ctor,max_size,options',
+    itertools.product(
+        [
+            LifoQueue
+        ],
+        [-1],
+        map_two_options([
+            (
+                {
+                    'is_async': False,
+                    'sequence': lifo_queue_sequence
+                },
+                {
+                    'is_async': True,
+                    'sequence': lifo_queue_sequence_got,
+                    'init_delay': 0.2
+                }
+            )
+        ])
+    )
+)
+def test_lifo_queue(
+    queue_ctor,
+    max_size: int,
+    options: Tuple[dict, dict]
+):
+    run(queue_ctor, max_size, options)
